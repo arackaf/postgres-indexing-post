11153f24aa4c3583d3e68e96354e02f744192cab
Adam Rackis <adamrackis@hotmail.com>
2025-08-13 00:41:57 -0500

    drizzle regenerate

--- drizzle/relations.ts
+++ drizzle/relations.ts
@@ -1,21 +1,13 @@
 import { relations } from "drizzle-orm/relations";
-import { publishers, books, activePromotions } from "./schema";
+import { publishers, books } from "./schema";
 
-export const booksRelations = relations(books, ({ one, many }) => ({
+export const booksRelations = relations(books, ({ one }) => ({
   publisher: one(publishers, {
     fields: [books.publisher],
     references: [publishers.id],
   }),
-  activePromotions: many(activePromotions),
 }));
 
 export const publishersRelations = relations(publishers, ({ many }) => ({
   books: many(books),
-}));
-
-export const activePromotionsRelations = relations(activePromotions, ({ one }) => ({
-  book: one(books, {
-    fields: [activePromotions.book],
-    references: [books.id],
-  }),
 }));
